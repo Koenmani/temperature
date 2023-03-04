40ce4c917684156cf3cf2ccfabdffbd5c643393a
koen <koenlan@begeleidingsplatform.nl>
2023-03-04 22:22:42 +0100

    Error in heating offset while heating - frontend change

--- homedashboard/rooms.js
+++ homedashboard/rooms.js
@@ -12,38 +12,42 @@ const Rooms = {
     open_close() {
 		result = false
 		
-		if (isDefined(this.item.radiator)){
-			if (this.item.radiator.length > 0){
-				if (this.item.radiator[0].open_close == 1){
-					result = true
-				}
-				else{
-					result = false
-				}
-			}
-		}
-		if (isDefined(this.item.airco)){
-			if (this.item.airco.length > 0){
-				if (this.item.airco[0].open_close == 1){
-					result = true
-				}
-				else{
-					result = false
+		if (isDefined(this.item.devices)){
+			if (this.item.devices.length > 0){
+				for (const d of this.item.devices) {
+					if (d.open_close == 1){
+						return true
+					}
+					else{
+						result = false
+					}
 				}
 			}
 		}
+		// if (isDefined(this.item.airco)){
+		// 	if (this.item.airco.length > 0){
+		// 		if (this.item.airco[0].open_close == 1){
+		// 			result = true
+		// 		}
+		// 		else{
+		// 			result = false
+		// 		}
+		// 	}
+		// }
 		return result
     },
   	open_close_error() {
 		result = false
 		
-		if (isDefined(this.item.radiator)){
-			if (this.item.radiator.length > 0){
-				if (this.item.radiator[0].open_close == 99){
-					result = true
-				}
-				else{
-					result = false
+		if (isDefined(this.item.devices)){
+			if (this.item.devices.length > 0){
+				for (const d of this.item.devices) {
+					if (d.open_close == 99){
+						return true
+					}
+					else{
+						result = false
+					}
 				}
 		}
 		return result
@@ -52,13 +56,17 @@ const Rooms = {
   	open_close_airco() {
 		result = false
 		
-		if (isDefined(this.item.airco)){
-			if (this.item.airco.length > 0){
-				if (this.item.airco[0].open_close == 1){
-					result = true
-				}
-				else{
-					result = false
+		if (isDefined(this.item.devices)){
+			if (this.item.devices.length > 0){
+				for (const d of this.item.devices) {
+					if (d.name == 'airco'){
+						if (d.open_close == 1){
+							return true
+						}
+						else{
+							return false
+						}
+					}
 				}
 			}
 		}
@@ -82,5 +90,5 @@ const Rooms = {
 		
 	}
   },
-  template: "<div :id='tidid' @click=\"this.select_kamer\" v-bind:class=\"[ open_close == true ? [ open_close_error ? 'radactive kamer error' : 'radactive kamer' ] : [ open_close_error ? 'radinactive kamer error' : 'radinactive kamer' ], [ this.item.handmatig == true ? 'handmatig' : 'automaat' ], [ open_close_airco ? 'airco' : '' ], [ this.item.lowbattery == true ? 'lowbat' : '' ] ]\"><div class=\"kamer_title\">{{ item.naam }}</div><div class=\"kamer_sub\"><div v-bind:class=\"[ this.item.insync == false ? 'kamer_curtemp error' : 'kamer_curtemp' ]\">{{ item.huidig }}&#176;</div><div class=\"kamer_newtemp\">{{ item.ingesteld }}&#176;</div></div></div>"
+  template: "<div :id='tidid' @click=\"this.select_kamer\" v-bind:class=\"[ open_close == true ? [ open_close_error ? 'radactive kamer error' : 'radactive kamer' ] : [ open_close_error ? 'radinactive kamer error' : 'radinactive kamer' ], [ this.item.handmatig == true ? 'handmatig' : this.item.exclude == true ? 'holiday' : 'automaat' ], [ open_close_airco ? 'airco' : '' ], [ this.item.lowbattery == true ? 'lowbat' : '' ] ]\"><div class=\"kamer_title\">{{ item.naam }}</div><div class=\"kamer_sub\"><div v-bind:class=\"[ this.item.insync == false ? 'kamer_curtemp error' : 'kamer_curtemp' ]\">{{ item.huidig }}&#176;</div><div class=\"kamer_newtemp\">{{ item.ingesteld }}&#176;</div></div></div>"
 }
